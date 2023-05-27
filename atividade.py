70114c3a407e768a840afaa63d0e75f644fb235e
Anninha11 <133766776+Anninha11@users.noreply.github.com>
2023-05-27 13:19:03 -0300

    Create atividade.py

---
+++ atividade.py
@@ -0,0 +1,13 @@
+#Dever para o dia 30/05
+!pip install streamlit
+import streamlit as st
+import pandas as pd
+
+def main():
+    st.title("Visualização de arquivo CSV")
+    file = st.file_uploader("ingrediente de comidas e alergias.csv")
+    if file is not None:
+        df = pd.read_csv(file)
+        st.write(df)
+if __name__ == "__main__":
+    main()
