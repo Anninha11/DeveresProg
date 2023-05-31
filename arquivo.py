00ffe3811f4edf3703e952a9639b2db31729c89e
Anninha11 <133766776+Anninha11@users.noreply.github.com>
2023-05-31 18:38:43 -0300

    Update arquivo.py

--- arquivo.py
+++ arquivo.py
@@ -1,9 +1,12 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 st.title("Análise de Arquivo CSV")
 csv_file = st.file_uploader("Selecione o arquivo CSV", type=["csv"])
 if csv_file is not None:
     data = pd.read_csv(csv_file)
     st.write(data)
-else:
-    st.write("Nenhum arquivo selecionado.")
+    st.subheader("Gráfico de Barras")
+    fig, ax = plt.subplots()
+    data.plot(kind="bar", ax=ax)
+    st.pyplot(fig)
