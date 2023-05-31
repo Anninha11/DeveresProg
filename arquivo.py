bcd0e7df087b07577a40071a733dd66cb589792d
Anninha11 <133766776+Anninha11@users.noreply.github.com>
2023-05-31 18:30:19 -0300

    Update arquivo.py

--- arquivo.py
+++ arquivo.py
@@ -1,7 +1,15 @@
 install matplotlib.pyplot
+import matplotlib.pyplot as plt
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+def main():
+    st.title("Visualização de arquivo CSV")
+    file = st.file_uploader("ingrediente de comidas e alergias.csv")
+    if file is not None:
+        df = pd.read_csv(file)
+        st.write(df)
+if __name__ == "__main__":
+    main()
 data = pd.read_csv('ingrediente de comidas e alergias.csv')
 category_counts = data[['Food Product', 'Main Ingredient', 'Sweetener', 'Fat/Oil', 'Seasoning', 'Allergens', 'Prediction']].apply(pd.Series.value_counts)
 plt.figure(figsize=(10, 6))
